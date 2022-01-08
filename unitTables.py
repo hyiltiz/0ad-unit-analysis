2b1fe968011663a044198112c3ccdf3717189b07
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2022-01-08 17:54:06 -0500

    Update copyright notice to 2022

--- unitTables.py
+++ unitTables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2015 Wildfire Games.
+# Copyright (C) 2015-2022 Wildfire Games.
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
